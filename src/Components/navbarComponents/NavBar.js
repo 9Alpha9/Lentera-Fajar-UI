ad850abd92ab4d4169022c1234f904c84098b648
Gilang Ramadhan <45790050+9Alpha9@users.noreply.github.com>
2022-07-04 21:44:36 +0700

    Change Navbar 2.0 ❤️

--- src/Components/navbarComponents/NavBar.js
+++ src/Components/navbarComponents/NavBar.js
@@ -1,34 +1,61 @@
-import React from 'react';
+import React, {useState} from "react";
 import './NavBar';
 import './NavBarStyles.css';
-import {FaBars, FaTimes} from "react-icons/fa";
-import {useRef} from "react";
+// import {FaBars, FaTimes} from "react-icons/fa";
+// import {useRef} from "react";
 
 function NavBar() {
-    const navRef = useRef();
+    const [active, setActive] = useState("nav__menu");
+  const [icon, setIcon] = useState("nav__toggler");
+  const navToggle = () => {
+    if (active === "nav__menu") {
+      setActive("nav__menu nav__active");
+    } else setActive("nav__menu");
 
-    const showNavBar = () => {
-        navRef.current.classList.toggle("responsive-nav");
-    }
-
-    return(
-        <header className='w-full'>
-            <h3>Logo</h3>
-            <nav ref={navRef}>
-                <a href="/#">Home</a>
-                <a href="/#">Home</a>
-                <a href="/#">Home</a>
-                <a href="/#">Home</a>
-                <a href="/#">Home</a>
-                <button className='nav-btn nav-close-btn' onClick={showNavBar}>
-                    <FaTimes />
-                </button>
-            </nav>
-            <button className='nav-btn' onClick={showNavBar}>
-                    <FaBars />
-                </button>
-        </header>
-    );
+    // Icon Toggler
+    if (icon === "nav__toggler") {
+      setIcon("nav__toggler toggle");
+    } else setIcon("nav__toggler");
+  };
+  return (
+    <nav className="nav">
+      <a href="/#" className="nav__brand">
+        Lentera Fajar Indonesia
+      </a>
+      <ul className={active}>
+        <li className="nav__item">
+          <a href="/#" className="nav__link">
+            Home
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="/#" className="nav__link">
+            Tentang Kami
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="/#" className="nav__link">
+            Artikel
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="/#" className="nav__link">
+            Guru
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="/#" className="nav__link">
+            Kegiatan Sekolah
+          </a>
+        </li>
+      </ul>
+      <div onClick={navToggle} className={icon}>
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar;
