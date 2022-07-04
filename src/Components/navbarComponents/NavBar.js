ca7956f6b11310f95d5ec8d29adbec1db8d95c26
Gilang Ramadhan <45790050+9Alpha9@users.noreply.github.com>
2022-07-04 20:02:40 +0700

    Navbar Vol 1 👌

--- src/Components/navbarComponents/NavBar.js
+++ src/Components/navbarComponents/NavBar.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import './NavBar';
 import './NavBarStyles.css';
-
+import {FaBars, FaTimes} from "react-icons/fa";
+import {useRef} from "react";
 
 function NavBar() {
-    
+    const navRef = useRef();
+
+    const showNavBar = () => {
+        navRef.current.classList.toggle("responsive-nav");
+    }
+
     return(
-       <div>
-        NavBar
-       </div>
+        <header className='w-full'>
+            <h3>Logo</h3>
+            <nav ref={navRef}>
+                <a href="/#">Home</a>
+                <a href="/#">Home</a>
+                <a href="/#">Home</a>
+                <a href="/#">Home</a>
+                <a href="/#">Home</a>
+                <button className='nav-btn nav-close-btn' onClick={showNavBar}>
+                    <FaTimes />
+                </button>
+            </nav>
+            <button className='nav-btn' onClick={showNavBar}>
+                    <FaBars />
+                </button>
+        </header>
     );
 }
 
